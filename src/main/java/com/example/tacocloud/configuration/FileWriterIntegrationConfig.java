2c5eae90b96dbd46c11523d4d99dfc5e240531f0
Martin Ströher <martin.andrade@townsq.com.br>
2023-07-18 22:28:28 -0300

    Added FileWriterIntegration configuration and interface, as well as dependency.

---
+++ src/main/java/com/example/tacocloud/configuration/FileWriterIntegrationConfig.java
@@ -0,0 +1,31 @@
+package com.example.tacocloud.configuration;
+
+import java.io.File;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.integration.annotation.ServiceActivator;
+import org.springframework.integration.annotation.Transformer;
+import org.springframework.integration.core.GenericTransformer;
+import org.springframework.integration.file.FileWritingMessageHandler;
+import org.springframework.integration.file.support.FileExistsMode;
+
+@Configuration
+public class FileWriterIntegrationConfig {
+
+    @Bean
+    @Transformer(inputChannel = "textInChannel", outputChannel = "fileWriterChannel")
+    public GenericTransformer<String, String> upperCaseTransformer() {
+        return text -> text.toUpperCase();
+    }
+
+    @Bean
+    @ServiceActivator(inputChannel = "fileWriterChannel")
+    public FileWritingMessageHandler fileWriter() {
+        var handler = new FileWritingMessageHandler(new File("/tmp/sia6/files"));
+        handler.setExpectReply(false);
+        handler.setFileExistsMode(FileExistsMode.APPEND);
+        handler.setAppendNewLine(true);
+
+        return handler;
+    }
+}
--- src/main/java/com/example/tacocloud/controller/OrderApiController.java
+++ src/main/java/com/example/tacocloud/controller/OrderApiController.java
@@ -5,6 +5,7 @@ import com.example.tacocloud.repository.OrderRepository;
 import com.example.tacocloud.service.OrderMessagingService;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
